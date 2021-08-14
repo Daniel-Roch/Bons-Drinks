292accfb96eb574f49ee6c99b14b7bbb0b46a92a
Daniel de Lemos Rocha <81339913+Daniel-Roch@users.noreply.github.com>
2021-08-14 18:42:20 -0300

    Criando pasta Forms.

---
+++ src/components/Forms/Forms.jsx
@@ -0,0 +1,66 @@
+import React,{Component} from 'react'
+import style from './Forms.module.css'
+import ImgForms from '../../assents/contact-pic.jpg'
+import Btn from '../Button/Button'
+import Input from '../Input/Input'
+import TextArea from '../TextArea/TextArea'
+
+export default class Forms extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            nome : "",
+            email: "",
+            mensagem: ""
+        }
+    }
+    handleInputChange = (event) =>{
+        switch (event.target.name){
+            case "nome" :
+                this.setState({nome: event.target.value})
+                break
+            case "email" : 
+                this.setState({email : event.target.value})
+                break
+            case "mensagem" :
+                this.setState({mensagem : event.target.value})
+                break
+            default :
+                throw new Error('Campo Invalido')
+        }
+    }
+    handleSubmit = (event) =>{
+        event.preventDefault()
+        let json = {
+            nome : this.state.nome,
+            email : this.state.email,
+            mensagem: this.state.mensagem
+        }
+        console.log(json)
+    }
+    render(){
+        return(<>
+        <h1 className={style.titleForms}>Contato</h1>
+        <form className={style.containerForms}>
+            <img src={ImgForms} className={style.imgForms} alt="back"/>
+            <Input type="text" name="nome" onChange={this.handleInputChange} className={style.formsNome}>Nome: </Input>
+            <Input type="email" name="email" onChange={this.handleInputChange} className={style.formsEmail}>E-mail: </Input>
+            <TextArea name="mensagem" onChange={this.handleInputChange} className={style.formsMensagem}>Mensagem: </TextArea>
+            <Btn className={style.btnForms} onClick={this.handleSubmit}>Enviar</Btn>
+        </form>
+        </>)
+    }
+}
+
+/* contructor(){
+        this.state = {
+            disabled : false
+        }   
+    }
+    disab = () =>{
+        this.setState({disabled : true})
+        setTimeout(()=>{
+            this.setState({disabled : false})
+        },6000)
+    }
+    <button disabled={this.state.disabled} onClick={this.disab}>Click</button> */
--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import style from './Intro.module.css'
+/* import {ReactComponent as LikeImg} from '../../assents/likeIcon.svg'
+<LikeImg/>  -- Dessa forma eu consigo importar imagem sem precisar jogar da forma {} (svg)
+consigo também agora fazer funções nele <LikeImg onClick={this.algo}/>*/
 
 export default class Intro extends Component {
     render() {
